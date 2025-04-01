56d9bcc908b482603b75006a8c56bdc376d374e8
udc2020 <udc-2020@hotmail.com>
2025-04-01 13:59:10 +0100

    prepare to published to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
+base:"/ofcode/",  
+resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
