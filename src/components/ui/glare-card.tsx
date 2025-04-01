2b899ef83f3a2831d05efb45af6e9955d21b1569
udc2020 <udc-2020@hotmail.com>
2025-04-01 14:06:11 +0100

    fix some issues

--- src/components/ui/glare-card.tsx
+++ src/components/ui/glare-card.tsx
@@ -41,18 +41,18 @@ export const GlareCard = ({
     "--transition": "var(--duration) var(--easing)",
   } as any;
 
-  const backgroundStyle = {
-    "--step": "5%",
-    "--foil-svg": `url("data:image/svg+xml,%3Csvg width='26' height='26' viewBox='0 0 26 26' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M2.99994 3.419C2.99994 3.419 21.6142 7.43646 22.7921 12.153C23.97 16.8695 3.41838 23.0306 3.41838 23.0306' stroke='white' stroke-width='5' stroke-miterlimit='3.86874' stroke-linecap='round' style='mix-blend-mode:darken'/%3E%3C/svg%3E")`,
-    "--pattern": "var(--foil-svg) center/100% no-repeat",
-    "--rainbow":
-      "repeating-linear-gradient( 0deg,rgb(255,119,115) calc(var(--step) * 1),rgba(255,237,95,1) 2),rgba(168,255,95,1) 3),rgba(131,255,247,1) 4),rgba(120,148,255,1) 5),rgb(216,117,255) 6),rgb(255,119,115) 7) ) 0% var(--bg-y)/200% 700% no-repeat",
-    "--diagonal":
-      "repeating-linear-gradient( 128deg,#0e152e 0%,hsl(180,10%,60%) 3.8%,hsl(180,10%,60%) 4.5%,hsl(180,10%,60%) 5.2%,#0e152e 10%,#0e152e 12% ) var(--bg-x) var(--bg-y)/300% no-repeat",
-    "--shade":
-      "radial-gradient( farthest-corner circle at var(--m-x) var(--m-y),rgba(255,255,255,0.1) 12%,rgba(255,255,255,0.15) 20%,rgba(255,255,255,0.25) 120% ) var(--bg-x) var(--bg-y)/300% no-repeat",
-    backgroundBlendMode: "hue, overlay",
-  };
+  // const backgroundStyle = {
+  //   "--step": "5%",
+  //   "--foil-svg": `url("data:image/svg+xml,%3Csvg width='26' height='26' viewBox='0 0 26 26' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M2.99994 3.419C2.99994 3.419 21.6142 7.43646 22.7921 12.153C23.97 16.8695 3.41838 23.0306 3.41838 23.0306' stroke='white' stroke-width='5' stroke-miterlimit='3.86874' stroke-linecap='round' style='mix-blend-mode:darken'/%3E%3C/svg%3E")`,
+  //   "--pattern": "var(--foil-svg) center/100% no-repeat",
+  //   "--rainbow":
+  //     "repeating-linear-gradient( 0deg,rgb(255,119,115) calc(var(--step) * 1),rgba(255,237,95,1) 2),rgba(168,255,95,1) 3),rgba(131,255,247,1) 4),rgba(120,148,255,1) 5),rgb(216,117,255) 6),rgb(255,119,115) 7) ) 0% var(--bg-y)/200% 700% no-repeat",
+  //   "--diagonal":
+  //     "repeating-linear-gradient( 128deg,#0e152e 0%,hsl(180,10%,60%) 3.8%,hsl(180,10%,60%) 4.5%,hsl(180,10%,60%) 5.2%,#0e152e 10%,#0e152e 12% ) var(--bg-x) var(--bg-y)/300% no-repeat",
+  //   "--shade":
+  //     "radial-gradient( farthest-corner circle at var(--m-x) var(--m-y),rgba(255,255,255,0.1) 12%,rgba(255,255,255,0.15) 20%,rgba(255,255,255,0.25) 120% ) var(--bg-x) var(--bg-y)/300% no-repeat",
+  //   backgroundBlendMode: "hue, overlay",
+  // };
 
   const updateStyles = () => {
     if (refElement.current) {
